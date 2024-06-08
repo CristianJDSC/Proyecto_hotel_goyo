aa9b55e449bae17735f63323bdc044d598d57c63
CristianJDSC <154480574+CristianJDSC@users.noreply.github.com>
2024-06-08 15:41:58 -0400

    Update proyecto_hotel.py

--- proyecto_hotel.py
+++ proyecto_hotel.py
@@ -1,6 +1,7 @@
 from time import sleep
 import pickle
 from enum import Enum
+from time import sleep
 
 class Categoria(Enum):
     SENCILLA = "Sencilla"
@@ -13,6 +14,52 @@ class Estado(Enum):
     OCUPADO = "Ocupado"
     LIBRE = "Libre"
     RESERVADO = "Reservado"
+
+
+def Mostrar_Habitaciones(habitaciones):
+    for i in range(len(habitaciones)):
+        print(f"Numero: {habitaciones[i][0]}")
+        print(f"Categoria: {habitaciones[i][1]}")
+        print(f"Estado: {habitaciones[i][4]}")
+                
+        if habitaciones[i][4]!=Estado.LIBRE.value:
+            print(f"Fecha de entrada: {habitaciones[i][2]}")
+            print(f"Fecha de salida: {habitaciones[i][3]}")
+        print()
+
+#Buscar habitaciones por numero o categoria
+def Busqueda_Habitacion(habitaciones, objetivo, tipo):      #El tipo viene siendo lo que se quiere buscar (numero o categoria)
+    found=False
+    for i in range(len(habitaciones)):
+        if objetivo==habitaciones[i][tipo]:
+            if found==False:
+                print("Las habitaciones encontradas son:"); print()
+            print(f"Numero: {habitaciones[i][0]}")
+            print(f"Categoria: {habitaciones[i][1]}")
+            print(f"Estado: {habitaciones[i][4]}")
+                        
+            if habitaciones[i][4]!=Estado.LIBRE.value:
+                print(f"Fecha de entrada: {habitaciones[i][2]}")
+                print(f"Fecha de salida: {habitaciones[i][3]}")
+            print()
+            found=True
+            
+    if found==False:
+        print("La habitacion introducida no se encuentra registrada"); print()
+    
+    
+def Ordenamiento(habitaciones, tipo):
+    for i in range(len(habitaciones)):
+        key = habitaciones[i]
+        k = i - 1
+        while k >= 0 and habitaciones[k][tipo] > key[tipo]:
+            habitaciones[k + 1] = habitaciones[k]
+
+            k -= 1
+
+        habitaciones[k + 1] = key
+
+    return habitaciones
 
         
     '''matriz_habitaciones=[[101,Categoria.SENCILLA.value,"","",Estado.LIBRE.value,"","",""],
@@ -50,24 +97,67 @@ def main():
         print("5.-", "Egresar usuario del hotel")
         print("6.-", "Reservar habitacion")
         print("7.-", "Cancelar reserva de habitacion")
-        print("8.-", "Actualizar los datos de una habitación:")
+        print("8.-", "Actualizar los datos de una habitación")
         print("9.-", "")
         print()
         opcion=str(input("Ingrese la opción que desee ejecutar: ")); print()
         
         #OPCIÓN 1
         if opcion=="1":
-            
-            print("Habitaciones:")
-            print()
-            
-            for i in range(len(matriz_habitaciones)):
-                print(f"Numero: {matriz_habitaciones[i][0]}")
-                print(f"Categoria: {matriz_habitaciones[i][1]}")
-                if matriz_habitaciones[i][4]!=Estado.LIBRE.value:
-                    print(f"Fecha de entrada: {matriz_habitaciones[i][2]}")
-                    print(f"Fecha de salida: {matriz_habitaciones[i][3]}")
-                print(f"Estado: {matriz_habitaciones[i][4]}")
+            print("Las habitaciones son:"); print()
+            Mostrar_Habitaciones(matriz_habitaciones)
+        
+        #OPCION 2
+        elif opcion=="2":
+            #Opciones de busqueda
+            while repeticion_menu:
+                print("MENÚ BUSQUEDA")
+                print("1.-", "Buscar por numero de habitacion")
+                print("2.-", "Buscar por categoria de habitacion")
+                print("0.-", "Regresar")
                 print()
+                opcion=str(input("Ingrese la opción que desee ejecutar: ")); print()
+                
+                if opcion=="1":
+                    while repeticion_menu:
+                        try:
+                            objetivo=int(input("Ingrese el numero de la habitacion: ")); print()
+                            Busqueda_Habitacion(matriz_habitaciones,objetivo,0)
+                            break
+                        except Exception as E:
+                            print("ERROR: Debe introducir un numero entero"); print()
+                elif opcion=="2":
+                    objetivo=str(input("Ingrese la categoria de la habitacion (Sencilla, Doble, Suite, Presidencial): ")); print()
+                    Busqueda_Habitacion(matriz_habitaciones,objetivo.capitalize(),1)
+                elif opcion=="0":
+                    break
+                else:
+                    print("Opcion invalida. Intentelo de nuevo"); print()
+                    sleep(3)
+
+        #OPCION 3
+        elif opcion=="3":
+            #Opciones de ordenamiento
+            while repeticion_menu:
+                print("MENÚ LISTADO")
+                print("1.-", "Mostrar por numero de habitacion de menor a mayor")
+                print("2.-", "Mostar por categoria de habitacion")
+                print("0.-", "Regresar")
+                print()
+                opcion=str(input("Ingrese la opción que desee ejecutar: ")); print()
+                
+                if opcion=="1":
+                    print("Las habitaciones son:"); print()
+                    matriz_ordenada=Ordenamiento(matriz_habitaciones,0)
+                    Mostrar_Habitaciones(matriz_ordenada)
+                elif opcion=="2":
+                    print("Las habitaciones son:"); print()
+                    matriz_ordenada=Ordenamiento(matriz_habitaciones,1)
+                    Mostrar_Habitaciones(matriz_ordenada)
+                elif opcion=="0":
+                    break
+                else:
+                    print("Opcion invalida. Intentelo de nuevo"); print()
+                    sleep(3)
     
 main()
